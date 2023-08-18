84e61cefcb1cd0306d950ffa9c90543392974121
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-18 08:39:19 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 
+
 const data = [
     {id : '1',
      tabTitle: "Tab 1",
      tabContent: 'Tab Content 1'
     },
+
+    {id : '2',
+    tabTitle: "Tab 2",
+    tabContent: 'Tab Content 2'
+   },
+
+   {id : '3',
+   tabTitle: "Tab 3",
+   tabContent: 'Tab Content 3'
+  }
+
 ]
